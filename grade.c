5eae70518734fb47530d4fc1ec1709a8a383a8f1
Pranathi-star <pranathik2001@gmail.com>
2020-08-21 22:44:24 +0530

    Updated code

--- grade.c
+++ grade.c
@@ -1,8 +1,20 @@
+//Algorithm:
+//Step 1: START
+//Step 2: Initialize float variables sub1, sub2, sub3, sub4, sub5, sub6 and avg.
+//Step 3: Take the user input for the marks in each of the six subjects.
+//Step 4: The control falls into the if block that checks if the marks in each subject are greater than 40. If the condition is True, print the result FAIL.
+//Step 5: If the condition is False, the control falls into the else block.
+//Step 6: Here, we know that the student has passed in all the subjects, we find the grade of the student and hence the control falls into the if-else ladder.
+//Step 7: Print the respective grade for the ranges of marks.
+//Step 8: STOP 
+
+
 #include<stdio.h>
 #include<stdlib.h>
 int main()
 {
     float sub1, sub2, sub3, sub4, sub5, sub6, avg;
+    printf("Enter the marks in the range of 0-100\n");
     printf("Enter the marks in Data Structures:");
     scanf("%f", &sub1);
     printf("Enter the marks in Discrete Mathematics:");
@@ -40,8 +52,8 @@ int main()
         else if(avg > 40 && avg <= 50){
             printf("Grade = C");
         }
-        else if(avg > 30 && avg <= 40){
-            printf("Grade = D");
+        else{
+			printf("Enter marks in the valid range only");
         }
 
 
