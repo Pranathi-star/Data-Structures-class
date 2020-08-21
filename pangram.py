6dbc17f13d8c1eaa07ebdbd552d26d42dbf80d4c
Pranathi-star <pranathik2001@gmail.com>
2020-08-21 12:59:36 +0530

    Updated pangram code

--- pangram.py
+++ pangram.py
@@ -1,27 +1,17 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[6]:
-
-
 #check whether a string is a pangram or not
 
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 
-def pangram_or_not(input_string: str) -> bool:
+def pangram_or_not(input_string):
     return not set(alphabet) - set(input_string.lower())
 
-result = pangram_or_not("The quick brown fox jumps over a lazy dog")
+input_string = input().strip()
+result = pangram_or_not(input_string)
 
 if result:
     print("The sentence is a pangram.")
 else:
     print("The sentence is not a pangram.")
-
-    
-
-
-# In[ ]:
 
 
 
