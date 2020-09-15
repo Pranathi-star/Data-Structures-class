da8edae235ebf777f7ed6acee04ab59b045f75d1
Pranathi-star <pranathik2001@gmail.com>
2020-09-15 13:43:40 +0530

    Selection sort in C

---
+++ selection_sort.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int elements[100], temp, no_of_elements, i, j, position;
+    printf("Enter the number of elements to be sorted: ");
+    scanf("%d", &no_of_elements);
+    // taking the input of array elements
+    for(i = 0; i < no_of_elements; i++){
+            printf("Enter element %d: ", i+1);
+            scanf("%d", &elements[i]);
+    }
+    // calculating the minimum and exchanging the values
+    for(i = 0; i < no_of_elements - 1; i++){
+        position = i;
+        for(j = i+1; j < no_of_elements; j++){
+            if(elements[position] > elements[j]){
+                    position = j;}
+            if(position != i){
+                temp = elements[i];
+                elements[i] = elements[position];
+                elements[position] = temp;}
+        }
+    }
+	// printing the array elements
+    for(i = 0; i < no_of_elements; i++){
+        printf("%d\n", elements[i]);}
+    return 0;
+}
